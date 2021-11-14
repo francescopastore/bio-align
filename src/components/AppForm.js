164f4b573e77b7b51d66c4345abad54f5983a61f
Francesco Pastore <francesco0599@hotmail.it>
2021-11-14 20:25:21 +0100

    Updated logic for FormInputWord

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -10,11 +10,27 @@ export default function AppForm() {
   return (
     <AppContainer>
       <FormSelectAlgorithm></FormSelectAlgorithm>
-      <FormInputWord></FormInputWord>
-      <FormInputWord></FormInputWord>
+      <FormInputWord
+        label="First word"
+        defaultValue="ATTACTC"
+        handleChange={handleFirstWord}
+      ></FormInputWord>
+      <FormInputWord
+        label="Second word"
+        defaultValue="ATATGTC"
+        handleChange={handleSecondWord}
+      ></FormInputWord>
       <FormInputNumber></FormInputNumber>
       <FormInputNumber></FormInputNumber>
       <FormInputNumber></FormInputNumber>
     </AppContainer>
   )
+}
+
+function handleFirstWord(value) {
+  console.log(value)
+}
+
+function handleSecondWord(value) {
+  console.log(value)
 }
--- src/components/AppForm/FormInputWord.js
+++ src/components/AppForm/FormInputWord.js
@@ -4,12 +4,25 @@ import InputAdornment from '@mui/material/InputAdornment'
 import FormControl from '@mui/material/FormControl'
 import EditIcon from '@mui/icons-material/Edit'
 
-export default function FormInputWord() {
+export default function FormInputWord(props) {
+  const [value, setValue] = React.useState(props.defaultValue)
+  const handleChange = (e) => {
+    const value = e.target.value.toUpperCase()
+    setValue(value)
+    props.handleChange(value)
+  }
+  const isError = () => {
+    return value.length > 20
+  }
   return (
     <FormControl fullWidth margin="normal">
       <TextField
-        label="Word"
+        onChange={handleChange}
+        label={props.label}
+        value={value}
+        error={isError()}
         variant="outlined"
+        helperText={isError() ? 'Max 20 alphabetic characters' : ''}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
