37e1efe80b3003084e5fdf6d7a3ff1b12ab2f9b5
Francesco Pastore <francesco0599@hotmail.it>
2021-11-17 21:21:58 +0100

    Fixed typography for AppTableCard

--- src/components/AppTableCard.js
+++ src/components/AppTableCard.js
@@ -1,11 +1,20 @@
 import * as React from 'react'
+import Typography from '@mui/material/Typography'
 
 import AppContainer from './AppContainer'
 
 export default function AppTableCard(props) {
+  const value = parseFloat(props.children.value)
+
   return (
     <AppContainer style={{ width: 20, mr: 3 }}>
-      {props.children.value}
+      {isNaN(value) ? (
+        <Typography align="center" sx={{ fontWeight: 'bold' }}>
+          {props.children.value}
+        </Typography>
+      ) : (
+        <Typography align="center">{value}</Typography>
+      )}
     </AppContainer>
   )
 }
