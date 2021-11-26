0ec1d4a884cc3c070482dfc86ba37d2a7d8fed9d
Francesco Pastore <francesco0599@hotmail.it>
2021-11-26 19:32:31 +0100

    Added logic for algorithms

--- src/logic/index.js
+++ src/logic/index.js
@@ -1,10 +1,30 @@
 const data = {
   algorithms: [
-    { label: 'Needleman-Wunsh' },
-    { label: 'Edit Distance' },
-    { label: 'Longest Common Subsequence' },
-    { label: 'Smith-Waterman' },
+    { label: 'Needleman-Wunsh', calculate: forTesting },
+    { label: 'Edit Distance', calculate: forTesting },
+    { label: 'Longest Common Subsequence', calculate: forTesting },
+    { label: 'Smith-Waterman', calculate: forTesting },
   ],
+}
+
+function forTesting(data) {
+  let table = []
+  let row = [{}] // first cell is empty
+  for (let i = 0; i < data.firstWord.length; i++) {
+    row.push({ value: data.firstWord.charAt(i) })
+  }
+  table.push(row)
+
+  for (let i = 0; i < data.secondWord.length; i++) {
+    row = []
+    row.push({ value: data.secondWord[i] })
+    for (var j = 0; j < data.firstWord.length; j++) {
+      row.push({ value: j, left: false, top: true, diag: true })
+    }
+    table.push(row)
+  }
+
+  return table
 }
 
 export default data
