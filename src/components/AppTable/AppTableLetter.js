71db0c8f8c32abc195a6faa31c51b8301e2c70b8
Francesco Pastore <francesco0599@hotmail.it>
2021-11-27 09:20:42 +0100

    Updated letter background

--- src/components/AppTable/AppTableLetter.js
+++ src/components/AppTable/AppTableLetter.js
@@ -1,12 +1,12 @@
 import * as React from 'react'
 import Typography from '@mui/material/Typography'
-import { grey } from '@mui/material/colors'
+import { blueGrey } from '@mui/material/colors'
 
 import AppTableCard from './AppTableCard'
 
 export default function AppTableLetter(props) {
   return (
-    <AppTableCard backgroundColor={grey['A100']}>
+    <AppTableCard backgroundColor={blueGrey['50']}>
       <Typography sx={{ fontWeight: 'bold' }}>{props.letter}</Typography>
     </AppTableCard>
   )
