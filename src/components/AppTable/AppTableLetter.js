03ec3c7488c8a7e1d29d3ff97edf1205848da965
Francesco Pastore <francesco0599@hotmail.it>
2021-11-28 19:28:55 +0100

    Updated letter color for more contrast

--- src/components/AppTable/AppTableLetter.js
+++ src/components/AppTable/AppTableLetter.js
@@ -6,7 +6,7 @@ import AppTableCard from './AppTableCard'
 
 export default function AppTableLetter(props) {
   return (
-    <AppTableCard backgroundColor={blueGrey['50']}>
+    <AppTableCard backgroundColor={blueGrey['A100']}>
       <Typography sx={{ fontWeight: 'bold' }}>{props.letter}</Typography>
     </AppTableCard>
   )
