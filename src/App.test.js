252c18ec3289330f18d4dbbed0ce411dd22f2a17
Francesco Pastore <francesco0599@hotmail.it>
2021-12-06 21:55:23 +0100

    Fixed old tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { Provider } from 'react-redux'
+
 import App from './App'
+import { store } from './store'
 
 it('renders correctly', () => {
-  const tree = renderer.create(<App></App>).toJSON()
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <App></App>
+      </Provider>
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
---
+++ src/components/AppForm/FormInputString.test.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+
+import FormInputString from './FormInputString'
+
+const fakeHandleChange = () => {}
+const fakeLabel = 'test'
+const fakeDefaultValue = 'ACTC'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <FormInputString
+        default={fakeDefaultValue}
+        label={fakeLabel}
+        handle={fakeHandleChange}
+      ></FormInputString>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+// test('default value param', () => {
+//   render(
+//     <FormInputWord
+//       defaultValue={fakeDefaultValue}
+//       label={fakeLabel}
+//       handleChange={fakeHandleChange}
+//     ></FormInputWord>
+//   )
+
+//   const input = screen.getByTestId('value-input').querySelector('input')
+//   expect(input).toHaveValue(fakeDefaultValue)
+// })
+
+// test('handle change param', () => {
+//   const newValue = 'TEST'
+//   const fakeHandleChange = (value) => {
+//     expect(value).toBe(newValue)
+//   }
+//   render(
+//     <FormInputWord
+//       defaultValue={fakeDefaultValue}
+//       label={fakeLabel}
+//       handleChange={fakeHandleChange}
+//     ></FormInputWord>
+//   )
+
+//   const input = screen.getByTestId('value-input').querySelector('input')
+//   expect(input).toBeInTheDocument()
+
+//   fireEvent.change(input, { target: { value: newValue } })
+//   expect(input).toHaveValue(newValue)
+// })
