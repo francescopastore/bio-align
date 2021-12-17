c5d6ea3467622ab0acca9a4df17bd0bbdbfb0c28
Francesco Pastore <francesco0599@hotmail.it>
2021-12-17 18:26:58 +0100

    Added tests for lcs

---
+++ src/logic/lcs.test.js
@@ -0,0 +1,53 @@
+import lcs from './lcs'
+
+const tests = [
+  {
+    data: {
+      firstString: 'ATTC',
+      secondString: 'GTGT',
+      matchWeight: 0,
+      mismatchWeight: 1,
+      gapWeight: 1,
+    },
+    expected: [
+      [{ value: 0 }, { value: 0 }, { value: 0 }, { value: 0 }, { value: 0 }],
+      [
+        { value: 0 },
+        { value: 0, left: false, top: false, diag: false },
+        { value: 0, left: false, top: false, diag: false },
+        { value: 0, left: false, top: false, diag: false },
+        { value: 0, left: false, top: false, diag: false },
+      ],
+      [
+        { value: 0 },
+        { value: 0, left: false, top: false, diag: false },
+        { value: 1, left: false, top: false, diag: true, isPath: true },
+        { value: 1, left: true, top: false, diag: false, isPath: true },
+        { value: 1, left: true, top: false, diag: true },
+      ],
+      [
+        { value: 0 },
+        { value: 0, left: false, top: false, diag: false },
+        { value: 1, left: false, top: true, diag: true },
+        { value: 1, left: true, top: true, diag: true },
+        { value: 2, left: false, top: false, diag: true, isPath: true },
+      ],
+      [
+        { value: 0 },
+        { value: 0, left: false, top: false, diag: false },
+        { value: 1, left: false, top: true, diag: false },
+        { value: 1, left: true, top: true, diag: true },
+        { value: 2, left: false, top: true, diag: false },
+      ],
+    ],
+  },
+]
+
+it('should calculate correctly', () => {
+  tests.forEach((v) => {
+    const actual = lcs(v.data)
+    console.log(v.expected)
+    console.log(actual)
+    expect(actual).toEqual(v.expected)
+  })
+})
