d225c3e90d0a7beb02967d105a5345c025d0fb70
Francesco Pastore <francesco0599@hotmail.it>
2021-12-19 19:57:51 +0100

    Added tests for AppTableLetter

---
+++ src/components/AppTable/AppTableLetter.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+
+import AppTableLetter from './AppTableLetter'
+
+const fakeLetter = 'A'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<AppTableLetter letter={fakeLetter}></AppTableLetter>)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('should render the letter given', () => {
+  render(<AppTableLetter letter={fakeLetter}></AppTableLetter>)
+
+  const div = screen.getByText(fakeLetter)
+  expect(div).toBeInTheDocument()
+})
