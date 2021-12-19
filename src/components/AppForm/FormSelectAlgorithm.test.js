8270aa0f3e94c726b969eef95cf696caa96a7f24
Francesco Pastore <francesco0599@hotmail.it>
2021-12-19 18:47:29 +0100

    Updated app snapshot

---
+++ src/components/AppForm/FormSelectAlgorithm.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+
+import FormSelectAlgorithm from './FormSelectAlgorithm'
+
+const fakeHandle = () => {}
+const fakeOptions = [
+  { label: 'Test 1' },
+  { label: 'Test 2' },
+  { label: 'Test 3' },
+]
+const fakeDefault = '0'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <FormSelectAlgorithm
+        default={fakeDefault}
+        options={fakeOptions}
+        handle={fakeHandle}
+      ></FormSelectAlgorithm>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('default value param', () => {
+  render(
+    <FormSelectAlgorithm
+      default={fakeDefault}
+      options={fakeOptions}
+      handle={fakeHandle}
+    ></FormSelectAlgorithm>
+  )
+
+  const input = screen.getByTestId('value-input').querySelector('input')
+  expect(input).toHaveValue(fakeDefault)
+})
